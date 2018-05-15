2200147e674b3c55de731496674299a3fe87691a
teacherigor <teacherigor6@gmail.com>
2018-05-15 21:07:52 +0300

    while (x <= y)

--- mytasks.js
+++ mytasks.js
@@ -1,5 +1,15 @@
-function myName() {
-	
+var x = 5;
+var y = 20;
+var text = ", ";
+
+// if (x <= y) {
+// 	console.log(x++ + text);
+// } else {
+// 	alert("Are you nuts?");
+// }
+
+
+while (x <= y) {
+	console.log(x++ + ", ");
 }
 
-var itemText = $('.item').text()
