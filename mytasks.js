075f36071e911a69c7b4f0133feb1123bf840d90
teacherigor <teacherigor6@gmail.com>
2018-05-19 20:24:19 +0300

    for {result = result + x + text}

--- mytasks.js
+++ mytasks.js
@@ -3,18 +3,8 @@ var y = 20;
 var text = ", ";
 var result = "";
 
-
-// if (x <= y) {
-// 	console.log(x++ + text);
-// } else {
-// 	alert("Are you nuts?");
-// }
-
-
-while (x <= y) {
-	result = result + x++ + text;
-	
+for(x; x <= y; x++) {
+result = result + x + text;
 }
 
 console.log(result);
-
