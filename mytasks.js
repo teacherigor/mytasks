b99ba07616de441ae8ea0ce2a29da2e528733fb8
teacherigor <teacherigor6@gmail.com>
2018-05-20 16:27:45 +0300

    var start = 5

--- mytasks.js
+++ mytasks.js
@@ -1,10 +1,20 @@
-var x = 5;
+var start = 5;
 var y = 20;
 var text = ", ";
 var result = "";
 
-for(x; x <= y; x++) {
-result = result + x + text;
+for(var x = start; x <= y; x++) {
+	result = result + x;
+	if (x < y) {
+		result = result + text;
+	}
 }
 
-console.log(result);
+if (start > y) {
+	console.log('Something is wrong, bro!');
+} else {
+	console.log(x);
+	console.log(result);	
+}
+
+// HW for x = 10; y = 0: printout = every number divided by 10; спочатку встовпчик, а потім врядочок
