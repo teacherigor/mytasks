0577f624958e3d1f4984b5a983b3991ccf9acd7a
teacherigor <teacherigor6@gmail.com>
2018-06-09 14:22:37 +0300

    topower function

--- mytasks.js
+++ mytasks.js
@@ -1,22 +1,10 @@
-var x = 10;
-var y = 0;
-var divisionColumn = "";
-var divisionRow = "";
-var separate = ", ";
-
-for (x; x > y; x--) {
-divisionColumn = x / 10;
-console.log(divisionColumn);
-divisionRow = divisionRow + divisionColumn;
-if (x > 1) divisionRow = divisionRow + separate;
-
+function numberToPower(number, power){
+	
+	return number * number * number; 
 }
 
-console.log(divisionRow);
+var fiveToThirdPower = numberToPower(5);
 
+console.log(fiveToThirdPower);
 
-
-
-
-
-// HW for x = 10; y = 0: printout = every number divided by 10; initially in a column, then in a row
+HW: реалізувати функцію, яка зводить число в степінь
