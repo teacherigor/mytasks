249c4e0cd13bd9085a4dddc0ec7caa593082bbc4
teacherigor <teacherigor6@gmail.com>
2018-06-16 14:13:51 +0300

    numberFactorial with for loop

--- mytasks.js
+++ mytasks.js
@@ -1,11 +1,20 @@
 function numberFactorial (number) {
+	var result = 1;
+		
+	for(number; number >= 1; number--) {
+		
+		result = result * number;
 
-	if (number > 0 && number <= 1) {
-		return 1;
-	} else {
-		return number * numberFactorial(number - 1);
+	/* result = result * 10;
+	result = result * 9;
+	result = result * 8;		 */
+		
+		// number--
 	}
-	}
+	return result;
+}
 
 var x = numberFactorial(10);
 console.log(x);
+
+// HW numberFactorial with while; multiply only down to the half of the number
