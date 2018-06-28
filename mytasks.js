54b39418a7a06936769490c107d5772f48b682af
teacherigor <teacherigor6@gmail.com>
2018-06-28 11:59:28 +0300

    initialA instead of initial_a

--- mytasks.js
+++ mytasks.js
@@ -1,16 +1,18 @@
 function numberOfNumbers(a, b) {
 	var counter = 0;
-	var initial_a = a;
+	var initialA = a;
 	
 	while(a > 1) {
 		if(a % b == 0) counter++;
 		a--;
 	}
 	
-	console.log('My numbers are ' + initial_a + ' and ' + b);
+	console.log('My numbers are ' + initialA + ' and ' + b);
 	
 	return counter;
 }
 
 var x = numberOfNumbers(15, 2);
 console.log(x);
+
+//
